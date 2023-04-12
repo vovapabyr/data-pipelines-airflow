8b8cd05162ce6f2f5f4578cf4d4f80fbe8697e8d
Volodymyr Pabyrivskyi <pabyrivskyi@gmail.com>
2023-04-12 17:55:40 +0300

    Add postgres migrations task.

--- weather_dag.py
+++ weather_dag.py
@@ -11,8 +11,11 @@ def _process_weather(ti):
     info = ti.xcom_pull("extract_data")
     timestamp = info["dt"]
     temp = info["main"]["temp"]
-    logging.info(f"Time: {timestamp}. Temp: {temp}")
-    return timestamp, temp
+    humidity = info["main"]["humidity"]
+    cloudiness = info["clouds"]["all"]
+    windSpeed = info["wind"]["speed"]
+    logging.info(f"Time: {timestamp}. Temp: {temp}. Humidity: {humidity}. Cloudiness: {cloudiness}. WindSpeed: {windSpeed}.")
+    return timestamp, temp, humidity, cloudiness, windSpeed
 
 with DAG(dag_id = "weather", schedule = "@daily", start_date = days_ago(2)) as dag:
     create_table_postgres_task = PostgresOperator(
@@ -25,9 +28,20 @@ with DAG(dag_id = "weather", schedule = "@daily", start_date = days_ago(2)) as d
         """
     )
 
+    run_migrations_postgres_task = PostgresOperator(
+        task_id = "run_migrations_postgres",
+        postgres_conn_id = "measurements_db",
+        sql = r"""
+            ALTER TABLE measurements
+            ADD COLUMN IF NOT EXISTS humidity FLOAT,
+            ADD COLUMN IF NOT EXISTS cloudiness FLOAT,
+            ADD COLUMN IF NOT EXISTS windSpeed FLOAT;
+        """
+    )
+
     extract_data = SimpleHttpOperator(
         task_id = "extract_data",
-        http_conn_id = "weather_api_conf",
+        http_conn_id = "weather_api_con",
         endpoint = "data/2.5/weather",
         data = {"appId": Variable.get("WEATHER_API_KEY"), "q": "Lviv"},
         method = "GET",
@@ -45,11 +59,14 @@ with DAG(dag_id = "weather", schedule = "@daily", start_date = days_ago(2)) as d
         task_id = "inject_data",
         postgres_conn_id = "measurements_db",
         sql = r"""
-            INSERT INTO measurements (execution_time, temperature) VALUES
+            INSERT INTO measurements (execution_time, temperature, humidity, cloudiness, windSpeed) VALUES
             (to_timestamp({{ti.xcom_pull(task_ids='process_data')[0]}}),
-            {{ti.xcom_pull(task_ids='process_data')[1]}});
+            {{ti.xcom_pull(task_ids='process_data')[1]}},
+            {{ti.xcom_pull(task_ids='process_data')[2]}},
+            {{ti.xcom_pull(task_ids='process_data')[3]}},
+            {{ti.xcom_pull(task_ids='process_data')[4]}});
         """,
         trigger_rule = 'none_failed'
     )
 
-    create_table_postgres_task >> extract_data >> process_data >> inject_data
+    create_table_postgres_task >> run_migrations_postgres_task >> extract_data >> process_data >> inject_data
